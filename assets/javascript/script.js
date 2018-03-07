391791ad3e1d8b1241584bb6370e20a4b9ef7c85
Trevor Johnson <johnsontrevor55@gmail.com>
2018-03-06 18:59:58 -0700

    made function for api call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,47 @@
 window.onload = function () {
 
+  //EU4CWjTbBYzh48LCPMWP7aXZBKdV2wAz
+
+  //data.rating
+  //data.url 
+
+  var getGiphs = function(topic) {
+    var url = 'https://api.giphy.com/v1/gifs/search?api_key=EU4CWjTbBYzh48LCPMWP7aXZBKdV2wAz';
+    var q = '&q=' + topic;
+    var limit = '&limit=10';
+    var finalUrl = url+q+limit;
+
+    ajaxRequest(finalUrl)
+  };
+
+  //makes our ajax request based on the value of url.
+  var ajaxRequest = function(url) {
+    $.ajax({
+      url: url,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response.data)
+      displayGiphs(response.data);
+    })
+  };
+
+  var displayGiphs = function(data) {
+    for (i = 0; i < data.length; i++) {
+      var img = $('<img>');
+      // var url = JSON.stringify(data[i].images.fixed_height_still.url);
+      // console.log(url)
+      img.attr('src', data[i].images.fixed_height_still.url);
+      $('.display-giphs').append(img)
+    }
+  };
+
   //These words will be used to get giphs associated with them.
   var topicsArray = ['Darth Vader','Donald Trump','The Lord of The Rings'];
 
   //creates a button for each index in topicsArray
   var createButtons = function() {
     for (i = 0; i < topicsArray.length; i++) {
-      var newButton = $(`<button type="button" class="btn btn-dark mr-2">${topicsArray[i]}</button>`);
+      var newButton = $(`<button type="button" class="btn btn-dark mr-2 get-giphs">${topicsArray[i]}</button>`);
 
       //adds to the new btn to the .button-container
       $('.button-container').append(newButton);
@@ -35,4 +70,18 @@ window.onload = function () {
     //re-adds the buttons to the screen with the new button
     createButtons();
   });
+
+
+  $(document).on('click', '.get-giphs', function() {
+
+    var btnValue = $(this).text();
+
+    //removes giphs
+    $('.display-giphs').empty();
+
+    //displays new giphs based on button
+    getGiphs(btnValue);
+  });
+
+
 }
